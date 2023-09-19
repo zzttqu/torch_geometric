c5103791391a07c984a7856131bb104268f8e6e3
zzttqu <zzttqu@gmail.com>
2023-09-19 13:29:54 +0800

    添加了pipfile，然后添加了tensorboard

--- train.py
+++ train.py
@@ -5,12 +5,12 @@ import networkx as nx
 from matplotlib import pyplot as plt
 import torch
 from torch_geometric.data import Data
-
 from torch.distributions import Categorical
 from torch_geometric.data import Data
 from GNNNet import GNNNet
 from GNNAgent import Agent, PPOMemory
 from envClass import EnvRun, StateCode
+from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == '__main__':
     # 设置显示
@@ -23,13 +23,16 @@ if __name__ == '__main__':
     work_cell_num = 6
     batch_size = 4
     agent_reward = 0
-    max_steps = 500
+    max_steps = 10
     total_step = 0
     epoch_step = 0
     memory = PPOMemory(batch_size, work_cell_num, function_num, 4, 2, device)
     env = EnvRun(1, work_cell_num=work_cell_num, function_num=function_num, device=device)
     agent = Agent(work_cell_num, function_num, batch_size=batch_size, n_epochs=64, mini_batch_size=4)
     # 如果不可视化节点就不用取返回值graph
+    # 加入tensorboard
+    writer = SummaryWriter(log_dir='logs')
+    writer.add_graph(agent.network, input_to_model=[new_state, new_map], verbose=False)
     graph = env.build_edge()
     work_function = env.get_work_cell_functions()
     weight = torch.tensor([1] * work_cell_num, dtype=torch.float).squeeze()
@@ -68,22 +71,22 @@ if __name__ == '__main__':
 
     # 神经网络要输出每个工作站的工作，功能和传输与否
 
-    # # 可视化
-    # node_states = nx.get_node_attributes(graph, 'state')
-    # node_function = nx.get_node_attributes(graph, 'function')
-    # nodes = nx.nodes(graph)
-    # edges = nx.edges(graph)
-    # node_labels = {}
-    # edge_labels = {}
-    # for node in nodes:
-    #     # 这里只用\n就可以换行了
-    #     node_labels[node] = f'{node}节点：\n 状态：{node_states[node]} \n 功能：{node_function[node]}'
-    #
-    # # print(node_labels)
-    # pos = nx.spring_layout(graph)
-    # nx.draw_networkx_nodes(graph, pos)
-    # nx.draw_networkx_labels(graph, pos, node_labels)
-    # # nx.draw_networkx_edges(graph, pos, connectionstyle="arc3,rad=0.2")
-    # nx.draw_networkx_edges(graph, pos)
+    # 可视化
+    node_states = nx.get_node_attributes(graph, 'state')
+    node_function = nx.get_node_attributes(graph, 'function')
+    nodes = nx.nodes(graph)
+    edges = nx.edges(graph)
+    node_labels = {}
+    edge_labels = {}
+    for node in nodes:
+        # 这里只用\n就可以换行了
+        node_labels[node] = f'{node}节点：\n 状态：{node_states[node]} \n 功能：{node_function[node]}'
 
-    # plt.show()
+    # print(node_labels)
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos, node_labels)
+    # nx.draw_networkx_edges(graph, pos, connectionstyle="arc3,rad=0.2")
+    nx.draw_networkx_edges(graph, pos)
+
+    plt.show()
